7d1ad725305604e3948897589a0d25e54a805786
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-03 09:22:42 +0100

    added a menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        System.out.println("--- Converter Menu ---");
+        System.out.println("1. Currency Converter ");
+        System.out.println("2. Temperature Converter");
+        System.out.println("3. Length Converter ");
+        System.out.println("0. Exit");
+        System.out.print("Choose converter type: ");
         }
     }
-}
+
