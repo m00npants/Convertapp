da31436737ea589509cf8260ea78bf24a87d25d4
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-03 10:47:54 +0100

    added some comments in the code

--- src/main/java/org/example/LengthConverter.java
+++ src/main/java/org/example/LengthConverter.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class LengthConverter {
     public void convert(Scanner scanner) {
+        //menu
         System.out.println("\n--- Length Converter ---");
         System.out.println("1. Meters to Kilometers");
         System.out.println("2. Kilometers to Meters");
@@ -12,13 +13,13 @@ public class LengthConverter {
         double length = scanner.nextDouble();
         double result = 0;
         String from = "", to = "";
-
+//converter
         switch (option) {
             case 1 -> { result = length / 1000; from = "meters"; to = "kilometers"; }
             case 2 -> { result = length * 1000; from = "kilometers"; to = "meters"; }
             default -> System.out.println("Invalid option.");
         }
-
+//results and time/date
         if (option == 1 || option == 2) {
             System.out.printf("Result: %.2f %s = %.2f %s\n", length, from, result, to);
             System.out.println("Converted at: " + DateTime.getTimestamp());
